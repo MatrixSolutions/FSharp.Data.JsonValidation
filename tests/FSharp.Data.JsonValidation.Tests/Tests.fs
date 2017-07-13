7ea6bd109d7e951eb47bb6aeb2bcc3a9edfe7f80
Nicholas Treu <ntreu@matrixformedia.com>
2017-07-12 20:57:17 -0400

    Convert NUnit tests to Expecto

--- tests/FSharp.Data.JsonValidation.Tests/Tests.fs
+++ tests/FSharp.Data.JsonValidation.Tests/Tests.fs
@@ -2,217 +2,191 @@ module FSharp.Data.JsonValidation.Tests
 
 open FSharp.Data
 open FSharp.Data.JsonValidation
-open NUnit.Framework
+open Expecto
 
 let valid = function
   | Valid _ -> ()
-  | result -> Assert.Fail (sprintf "Expected valid, got %A" result)
+  | result -> failwith <| sprintf "Expected valid, got %A" result
 
-let invalid = function
+let invalid = function 
   | Invalid _ -> ()
-  | result -> Assert.Fail (sprintf "Expected invalid, got %A" result)
+  | result -> failwith <| sprintf "Expected invalid, got %A" result 
 
-[<Test>]
-let ``the docs example works as written (a fat test, but bad code in docs is annoying)`` () =
-   let schema = ExactlyOneOf [JsonValue.String "hi"; JsonValue.Number 42M]
-   let value = JsonValue.String "goodbye"
+let toJsonString str = JsonValue.String str  
+let toJsonNumber num = num |> (JsonValue.Number << decimal)
+let toNullJsonArray count = Array.replicate count JsonValue.Null |> JsonValue.Array
+let toJsonArray array = JsonValue.Array array 
 
-   Assert.AreEqual("Invalid \"Expected value to be one of [\"hi\"; 42] but was \"goodbye\"\"", sprintf "%A" (validate schema value))
+[<Tests>]
+let tests =
+  testList "FSharp.Data.JsonValidation tests" [
+    
+    testCase "the docs example works as written (a fat test, but bad code in docs is annoying)" <| fun _ -> 
+      let schema = ExactlyOneOf [JsonValue.String "hi"; JsonValue.Number 42M]
+      let value = JsonValue.String "goodbye"
 
-   valid <| validate schema (JsonValue.Number 42M)
+      Expect.equal "Invalid \"Expected value to be one of [\"hi\"; 42] but was \"goodbye\"\"" (sprintf "%A" <| validate schema value) "there is an invalid result"
 
-[<Test>]
-let ``the tutorial example works as written (a fat test, but bad code in docs is annoying)`` () =
-  let someNumbers = ArrayWhose [ItemsMatch AnyNumber]
+    testCase "the tutorial example works as written (a fat test, but bad code in docs is annnoying" <| fun _ -> 
+      let someNumbers = ArrayWhose [ ItemsMatch AnyNumber ]
+      let looksLikeAnEmail (value: string) = 
+        value.Contains "@" && value.Contains "." && value.Length > 3
 
-  let looksLikeAnEmail (value: string) =
-    value.Contains "@" && value.Contains "." && value.Length > 3
+      let person =
+        ObjectWhere 
+          [
+            "name" .= StringThat [ IsNotEmpty ]
+            "favoriteNumbers" .= someNumbers
+            "email" .?= StringThat [ MeetsCriteria ("be an email", looksLikeAnEmail) ]
+          ]
+      
+      let okayPerson = """
+        {
+          "name": "Saul Goodman",
+          "favoriteNumbers": [747, 737000]
+        }"""
 
+      valid <| validate person (JsonValue.Parse okayPerson)
 
-  let person =
-    ObjectWhere
-      [
-        "name" .= StringThat [IsNotEmpty]
-        "favoriteNumbers" .= someNumbers
-        "email" .?= StringThat [MeetsCriteria ("be an email", looksLikeAnEmail)]
-      ]
+      let emptyNamePerson = """
+        {
+          "name": "",
+          "favoriteNumbers": [],
+          "email": "mysterious@haunted.house"
+        }"""
+        
+      let badEmail = """
+        {
+          "name": "Jimmy M.",
+          "favoriteNumbers": [],
+          "email": "not-really-an-email"
+        }"""
 
-  let okayPerson =
-    """
-    {
-      "name": "Saul Goodman",
-      "favoriteNumbers": [747, 737000]
-    }
-    """
+      Expect.equal "Invalid \".name Expected string to not be empty\"" (sprintf "%A" <| validate person (JsonValue.Parse emptyNamePerson)) "expected string to not be empty"
+      Expect.equal "Invalid \".email Expected string \"not-really-an-email\" to be an email\"" (sprintf "%A" <| validate person (JsonValue.Parse badEmail)) "expected to be an email string"
+  
+    testCase "anything matches anything" <| fun _ -> 
+      valid <| validate Anything (toJsonString "hi")
+      valid <| validate Anything (toJsonNumber 42)
+      valid <| validate Anything (JsonValue.Array [||])
+      valid <| validate Anything (JsonValue.Array [||])
+  
+    testCase "Exactly only matches exactly what it is given" <| fun _ ->
+      valid <| validate (Exactly <| toJsonString "hi") (toJsonString "hi")
+      invalid <| validate (Exactly <| toJsonString "hi") (toJsonString "hi2")
+      invalid <| validate (Exactly <| toJsonString "hi") JsonValue.Null
 
-  valid <| validate person (JsonValue.Parse okayPerson)
+    testCase "ExactlyOneOf matches any of the values that it is given" <| fun _ ->
+      let schema = ExactlyOneOf [ toJsonString "hi"; toJsonNumber 42 ]
 
-  let emptyNamePerson =
-    """
-    {
-      "name": "",
-      "favoriteNumbers": [],
-      "email": "mysterious@haunted.house"
-    }
-    """
+      valid <| validate schema (toJsonString "hi")
+      valid <| validate schema (toJsonNumber 42)
+      invalid <| validate schema JsonValue.Null
+      invalid <| validate schema (toJsonString "hi2")
 
-  Assert.AreEqual("Invalid \".name Expected string to not be empty\"",  sprintf "%A" <| validate person (JsonValue.Parse emptyNamePerson))
+    testCase "a JSON Number is AnyNumber" <| fun _ -> 
+      valid <| validate AnyNumber (toJsonNumber 42)
 
+    testCase "something other than a JSON number is not AnyNumber" <| fun _ ->
+      invalid <| validate AnyNumber (toJsonString "I am a string")
 
-  let badEmail =
-    """
-    {
-      "name": "Jimmy M.",
-      "favoriteNumbers": [],
-      "email": "not-really-an-email"
-    }
-    """
+    testCase "a JSON string is AnyString" <| fun _ -> 
+      valid <| validate AnyString (toJsonString "Hi")
 
-  Assert.AreEqual("Invalid \".email Expected string \"not-really-an-email\" to be an email\"", sprintf "%A" <| validate person (JsonValue.Parse badEmail))
+    testCase "something other than a JSON string is not AnyString" <| fun _ ->
+      invalid <| validate AnyString (toJsonNumber 42)
 
-[<Test>]
-let ``Anything matches anything`` () =
-  valid <| validate Anything (JsonValue.String "hi")
-  valid <| validate Anything (JsonValue.Number 42M)
-  valid <| validate Anything (JsonValue.Array [||])
-  valid <| validate Anything (JsonValue.Record [||])
+    testCase "StringThat [ IsNotEmpty ] matches non-empty strings" <| fun _ -> 
+      valid <| validate (StringThat [ IsNotEmpty ]) (toJsonString "hi")
+      invalid <| validate (StringThat [ IsNotEmpty ]) (toJsonString "")
 
-[<Test>]
-let ``Exactly only matches exactly what it's given`` () =
-  valid <| validate (Exactly <| JsonValue.String "hi") (JsonValue.String "hi")
-  invalid <| validate (Exactly <| JsonValue.String "hi") (JsonValue.String "hi2")
-  invalid <| validate (Exactly <| JsonValue.String "hi") JsonValue.Null
+    testCase "ObjectWhere [...] must have required keys that match the given schema" <| fun _ ->
+      let schema = ObjectWhere [ "foo" .= AnyString ]
 
-[<Test>]
-let ``ExactlyOneOf matches any of the values it's given`` () =
-  let schema = ExactlyOneOf [JsonValue.String "hi"; JsonValue.Number 42M]
+      invalid <| validate schema (JsonValue.Parse """ { "foo": 42 } """)
+      valid <| validate schema (JsonValue.Parse """ { "foo": "bar" } """)
 
-  valid <| validate schema (JsonValue.String "hi")
-  valid <| validate schema (JsonValue.Number 42M)
-  invalid <| validate schema JsonValue.Null
-  invalid <| validate schema (JsonValue.String "hi2")
+    testCase "`ObjectWhere [...] can omit optional keys" <| fun _ -> 
+      let schema = ObjectWhere [ "foo" .?= AnyString ]
 
-[<Test>]
-let ``a JSON number is AnyNumber`` () =
-  valid <| validate AnyNumber (JsonValue.Number 42M)
+      valid <| validate schema (JsonValue.Parse """ { } """)
+      valid <| validate schema (JsonValue.Parse """ { "foo": "bar" } """)
 
-[<Test>]
-let ``something other than a JSON number is not AnyNumber`` () =
-  invalid <| validate AnyNumber (JsonValue.String "Foo")
+    testCase "ObjectWhere [...] must meet the value schema when optional keys are given" <| fun _ -> 
+      let schema = ObjectWhere [ "foo" .?= AnyString ]
 
-[<Test>]
-let ``a JSON string is AnyString`` () =
-  valid <| validate AnyString (JsonValue.String "Hi")
+      invalid <| validate schema (JsonValue.Parse """ { "foo": 42 } """)
 
-[<Test>]
-let ``something other than a JSON string is not AnyString`` () =
-  invalid <| validate AnyString (JsonValue.Number 42M)
+    testCase "ObjectWhere [...] ignores keys that aren't mentioned in the schema" <| fun _ -> 
+      let schema = ObjectWhere []
 
-[<Test>]
-let ``StringThat [IsNotEmpty] matches non-empty strings`` () =
-  valid <| validate (StringThat [IsNotEmpty]) (JsonValue.String "hi")
-  invalid <| validate (StringThat [IsNotEmpty]) (JsonValue.String "")
+      valid <| validate schema (JsonValue.Parse """ { "foo": 42 } """)
 
-[<Test>]
-let ``ObjectWhere [...] must have required keys that match the given schema`` () =
-  let schema = ObjectWhere [ "foo" .= AnyString ]
+    testCase "ArrayWhose [LengthIsAtLeast n] works" <| fun _ -> 
+      let schema = ArrayWhose [LengthIsAtLeast 4]
 
-  invalid <| validate schema (JsonValue.Parse """ { "foo": 42 } """)
-  valid <| validate schema (JsonValue.Parse """ { "foo": "bar" } """)
+      invalid <| validate schema (toNullJsonArray 3)
+      valid <| validate schema (toNullJsonArray 4)
+      valid <| validate schema (toNullJsonArray 5)
 
-[<Test>]
-let ``ObjectWhere [...] can omit optional keys`` () =
-  let schema = ObjectWhere [ "foo" .?= AnyString ]
+    testCase "ArrayWhose [LengthIsAtMost n] works" <| fun _ -> 
+      let schema = ArrayWhose [LengthIsAtMost 4]
 
-  valid <| validate schema (JsonValue.Parse """ { } """)
-  valid <| validate schema (JsonValue.Parse """ { "foo": "bar" } """)
+      invalid <| validate schema (toNullJsonArray 3)
+      valid <| validate schema (toNullJsonArray 4)
+      valid <| validate schema (toNullJsonArray 5)
+    
+    testCase "ArrayWhose [LengthIsExactly n] works" <| fun _ -> 
+      let schema = ArrayWhose [LengthIsExactly 4]
 
-[<Test>]
-let ``ObjectWhere [...] must meet the value schema when optional keys are given`` () =
-  let schema = ObjectWhere [ "foo" .?= AnyString ]
+      invalid <| validate schema (toNullJsonArray 3)
+      valid <| validate schema (toNullJsonArray 4)
+      valid <| validate schema (toNullJsonArray 5)
 
-  invalid <| validate schema (JsonValue.Parse """ { "foo": 42 } """)
+    testCase "ArrayWhose [ItemsMatch schema] ensures items all match" <| fun _ ->
+      let schema = ArrayWhose [ ItemsMatch AnyNumber ]
 
+      invalid <| validate schema (toJsonArray [| JsonValue.Null |])
+      invalid <| validate schema (toJsonArray [| toJsonNumber 42; JsonValue.Null |])
+      valid <| validate schema (JsonValue.Array [| toJsonNumber 42; toJsonNumber 0 |])
 
-[<Test>]
-let ``ObjectWhere [...] ignores keys that aren't mentioned in the schema`` () =
-  let schema = ObjectWhere []
+    testCase "Either [...] ensures any item matches" <| fun _ -> 
+      let schema = Either [ Exactly <| toJsonNumber 42; AnyString ]
 
-  valid <| validate schema (JsonValue.Parse """ { "foo": 42 } """)
+      valid <| validate schema (toJsonString "42")
+      invalid <| validate schema (toJsonNumber 99)
+      valid <| validate schema (toJsonNumber 42)
 
+    testCase "Delay matches allowing recursive data structures" <| fun _ ->
+      let rec memory = 
+        ObjectWhere
+          [
+            "subject" .= StringThat [IsNotEmpty]
+            "linkedMemory" .?= Delay (fun () -> memory)
+          ]
+      valid <| validate memory (JsonValue.Parse """ { "subject": "Lone Memory" } """)
 
-[<Test>]
-let ``ArrayWhose [LengthIsAtLeast n] works`` () =
-  let schema = ArrayWhose [LengthIsAtLeast 4]
+      """
+      {
+        "subject": "I link",
+        "linkedMemory": {
+          "subject": "I'm linked to"
+        }
+      }
+      """
+        |> JsonValue.Parse
+        |> validate memory
+        |> valid
 
-  invalid <| validate schema (JsonValue.Array <| Array.replicate 3 JsonValue.Null)
-  valid <| validate schema (JsonValue.Array <| Array.replicate 4 JsonValue.Null)
-  valid <| validate schema (JsonValue.Array <| Array.replicate 5 JsonValue.Null)
-
-[<Test>]
-let ``ArrayWhose [LengthIsAtMost n] works`` () =
-  let schema = ArrayWhose [LengthIsAtMost 4]
-
-  valid <| validate schema (JsonValue.Array <| Array.replicate 3 JsonValue.Null)
-  valid <| validate schema (JsonValue.Array <| Array.replicate 4 JsonValue.Null)
-  invalid <| validate schema (JsonValue.Array <| Array.replicate 5 JsonValue.Null)
-
-[<Test>]
-let ``ArrayWhose [LengthIsExactly n] works`` () =
-  let schema = ArrayWhose [LengthIsExactly 4]
-
-  invalid <| validate schema (JsonValue.Array <| Array.replicate 3 JsonValue.Null)
-  valid <| validate schema (JsonValue.Array <| Array.replicate 4 JsonValue.Null)
-  invalid <| validate schema (JsonValue.Array <| Array.replicate 5 JsonValue.Null)
-
-
-[<Test>]
-let ``ArrayWhose [ItemsMatch schema] ensures items all match`` () =
-  let schema = ArrayWhose [ItemsMatch AnyNumber]
-
-  invalid <| validate schema (JsonValue.Array [|JsonValue.Null|])
-  invalid <| validate schema (JsonValue.Array [|JsonValue.Number 42M; JsonValue.Null|])
-  valid <| validate schema (JsonValue.Array [|JsonValue.Number 42M; JsonValue.Number 0M|])
-
-[<Test>]
-let ``Either [...] ensures any item matches`` () =
-  let schema = Either [Exactly <| JsonValue.Number 42M; AnyString]
-
-  valid <| validate schema (JsonValue.String "42")
-  invalid <| validate schema (JsonValue.Number 99M)
-  valid <| validate schema (JsonValue.Number 42M)
-
-[<Test>]
-let ``Delay matches allowing recursive data structures`` () =
-  let rec memory =
-    ObjectWhere
-      [
-        "subject" .= StringThat [IsNotEmpty]
-        "linkedMemory" .?= Delay (fun () -> memory)
-      ]
-
-  valid <| validate memory (JsonValue.Parse """ { "subject": "Lone Memory" } """)
-
-  """
-  {
-    "subject": "I link",
-    "linkedMemory": {
-      "subject": "I'm linked to"
-    }
-  }
-  """
-    |> JsonValue.Parse
-    |> validate memory
-    |> valid
-
-  """
-  {
-    "subject": "I link",
-    "linkedMemory": {
-      "subject": 42
-    }
-  }
-  """
-    |> JsonValue.Parse
-    |> validate memory
-    |> invalid
+      """
+      {
+        "subject": "I link",
+        "linkedMemory": {
+          "subject": 42
+        }
+      }
+      """
+        |> JsonValue.Parse
+        |> validate memory
+        |> invalid
+  ]
